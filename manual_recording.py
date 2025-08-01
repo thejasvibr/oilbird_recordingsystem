9c3662133ebb6718f7d07d6e0e502a1f907fa50e
tbeleyur <tbeleyur@orn.mpg.de>
2025-08-01 20:18:24 +0200

    Add calibration data too. Device calibration measurements done in a bit of ac hurry so not all channels characterised.

--- manual_recording.py
+++ manual_recording.py
@@ -8,6 +8,9 @@ from tascam_finder import get_device_indexnumber
 
 import argparse
 help_text = """
+
+file_prefix : text to add at the beginning of a file
+
 recdurn : duration of recording to be made in seconds.
 
 hostapi: the host API to use for the audio recording. Either MME, WASAPI, or WDM-KS, defaults to MME
@@ -17,6 +20,7 @@ device: the name of the device as it appears on sd.query_devices(), Defaults to 
 
 parser = argparse.ArgumentParser(description=help_text,)
 parser.add_argument('-hostapi', type=str, help=help_text, default='MME')
+parser.add_argument('-file_prefix', type=str, default='')
 parser.add_argument('-rec_durn', type=float, help=help_text, default=3)
 parser.add_argument('-devicename', type=str, help=help_text, 
 default='Microphone (US-16x08)')
@@ -25,7 +29,7 @@ args = parser.parse_args()
 
 device_name = args.devicename
 dev_hostapi = args.hostapi
-
+prefix = args.file_prefix
 
 device_index = get_device_indexnumber(device_name=device_name, hostapi=dev_hostapi)
 fs = 96000 # sampling rate in Hz
@@ -37,7 +41,7 @@ audio = sd.rec(int(fs*recording_durn), samplerate=fs,
 endtime = dt.datetime.now()
 print('....recording done now....')
 # save the end time in LOCAL timesystem
-recname = 'oilbird_multichaudio_' + f'{dev_hostapi}-API_'+'rec-endtime_'+endtime.strftime("%Y-%m-%d_%H-%M-%S")+'.wav'
+recname = args.file_prefix + '_oilbird_multichaudio_' + f'{dev_hostapi}-API_'+'rec-endtime_'+endtime.strftime("%Y-%m-%d_%H-%M-%S")+'.wav'
 
 print('writing file....')
 sf.write(f'{recname}', audio, fs)
